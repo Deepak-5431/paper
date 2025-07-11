08b18da65a8e1480a0939b26d990761ac9552a66
Deepak <130539289+Deepak-5431@users.noreply.github.com>
2025-07-11 10:51:58 +0530

    Update page1.jsx

--- client/src/pages/page1.jsx
+++ client/src/pages/page1.jsx
@@ -377,7 +377,7 @@ const Page1 = () => {
     setLoginError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('http://localhost:5000/api/login-local', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
